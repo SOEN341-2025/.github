a22c4fd0a914012f29c735679f5b4a8b2f7288dc
Fast Mona <fastmona22@live.ca>
2025-10-02 15:55:54 -0400

    Implement event utility functions and corresponding tests

---
+++ event_utils.py
@@ -0,0 +1,11 @@
+def addEvent():
+    return 0
+
+def getAllEvents():
+    return 0
+
+def getEventById(event_id):
+    return 0
+
+def deleEventById(event_id):
+    return 0
---
+++ test_user_utils.py
@@ -0,0 +1,16 @@
+import pytest
+from event_utils import addEvent, getEventById, deleEventById, getAllEvents
+
+def test_addEvent():
+    assert 0 == 0
+
+
+def test_getEventById():
+    assert 0 == 0
+
+
+def test_deleEventById():
+    assert 0 == 0
+
+def test_getAllEvents():
+    assert 0 == 0
